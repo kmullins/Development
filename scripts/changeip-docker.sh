76cfc9d4f01aa8171365c100277494489726519d
kmullins <kmmmullins@gmail.com>
2017-11-17 12:06:57 -0500

    changeip-docker second pass - not done

--- scripts/changeip-docker.sh
+++ scripts/changeip-docker.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 # Changeip.sh is used to change the IP's in i2b2
 
-#var1=`grep Reverse /etc/httpd/conf.d/i2b2_proxy.conf | awk -F/   '{print $5}' `
-var1=`docker exec i2b2-web /bin/bash -c " grep Reverse /etc/httpd/conf.d/i2b2_proxy.conf"`
-leng=`expr length "$var1"`
-checklength=${#var1}
-cl=`expr ${checklength}`
-
+### Functions
 function changeips (){ 
                          echo " in changeips ... "
 			 echo " ..."
@@ -36,55 +31,72 @@ function changeips (){
 }
 
 if [ -z $1 ]
-then
-	echo "If you are on AWS you should pass in the external IP address as P1. The program will use your current ip address"
-        IP=$(ip addr | grep 'state UP' -A2 | tail -n1 | awk '{print $2}' | cut -f1  -d'/')
-        echo "No P1 - Using IP: $IP"
-else
+   then
+       if [ -f /sys/hypervisor/uuid ] && [ `head -c 3 /sys/hypervisor/uuid` == ec2 ]; then
+             AWS="yes"
+  	     echo "You did not pass in an IP address to use for this changeip program."
+     	     echo "This instance is running on AWS which requires the external address defined and "
+     	     echo "passed into the program. Without defining a external IP address the script will "
+ 	     echo "use the private IP address which will not be accessable via the internet"
+     	     echo " "
+     	     echo "Please exit and pass a P1 variable into the program via ...."
+     	     echo " ....  sudo sh changeip.sh 123.456.789.123 ...." 
+       else
+             AWS="no"
+	     IP=$(ip addr | grep 'state UP' -A2 | tail -n1 | grep -oP '\d+\.\d+\.\d+\.\d+' | head -1) 
+	     echo " "
+	     echo "You did not pass in an IP address to use for this program."
+	     echo "This instance will use the current interal address for this process which is"
+   	     echo "  ....  $IP  ...."
+     	     echo " "
+     	     echo "If this IP address is incorrect, Please exit and pass a P1 variable into the program via ... " 
+    	     echo "     .... run_docker_network.sh 123.456.789.123  ...." 
+    	     echo " "
+       fi
+       
+       read -p "Do you want to EXIT and define an IP variable  (yes or no)" choice
+         case "$choice" in
+            Yes|YES|yes|y) echo "Yes I want to EXIT"
+         	echo " "
+		echo "Exiting "
+        	echo " "
+	        exit  ;;
+	    No|NO|no|n)    echo "OK continuing with installation";;
+	    *) echo "Bad choice - Try again" ;;
+	 esac
+  else
+
+     IP=$1
+     echo " "
+     echo "Using given IP to compare against current IP configuration: $1"
+     echo " "
+  fi
+
+
+  var1=`docker exec i2b2-web /bin/bash -c " grep Reverse /etc/httpd/conf.d/i2b2_proxy.conf | grep -oP '\d+\.\d+\.\d+\.\d+' | head -1) '"
+
+
+if [ ${var} = ${IP} ] 
+   then
+	echo "$IP and $var1 are the same"
+   else 
+
+	echo "$IP and $var1 are NOT the same"
+	echo "$IP and $var1 are NOT the same"
+	echo "$IP and $var1 are NOT the same"
+
 	
-        IP=$1
-        echo "using given IP: $1"
-fi
-
-
-if [ $cl -gt 15 ];
-then
-	echo " The IP includes a reference to a port $var1 "
-	echo " Removing port reference"
-	oldip=` echo $var1 | awk -F: '{print $1}' `
-	echo $oldip
-else
-	echo " Length is ok $leng"
-	oldip="$var1"
-	echo "OLDIP ... $oldip"
-
-fi
-
-
-
-if [ ${IP} = ${oldip} ] 
-then
-	echo "$IP and $oldip are the same"
-else 
-
-	echo "$IP and $oldip are NOT the same"
-	echo "$IP and $oldip are NOT the same"
-	echo "$IP and $oldip are NOT the same"
-
-	
-read -p "Do you want to update IP's (yes or no)" choice
-case "$choice" in
+   read -p "Do you want to update IP's (yes or no)" choice
+   case "$choice" in
 	 Yes|YES|yes|y) echo "Yes, update the IPs"
-		 
 		 echo " Updating"
 
-		 changeips
+####		 changeips
                  exit
-
 		 ;;
 	 No|NO|no|n)    echo "No, do not change the IPs";;
 	 *) echo "Bad choice" ;;
-esac
+   esac
 fi
 
 
