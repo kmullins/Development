7064b16b5f517ff2a1742c1acbe27a1b6c67d35a
kmullins <kmmmullins@gmail.com>
2017-11-30 15:31:29 -0500

    Check-i2b2.sh for non docker instances

--- scripts/check-i2b2.sh
+++ scripts/check-i2b2.sh
@@ -1,48 +1,14 @@
 #!/bin/bash
 
-webcl=/var/www/html/webclient
-wildf=/opt/local/i2b2-quickstart/local/wildfly-9.0.1.Final
-webht=/etc/httpd/conf.d/
-IP=$(ip addr | grep 'state UP' -A2 | tail -n1 | awk '{print $2}' | cut -f1  -d'/')
-selinuxstatus=`getenforce`
-
-
-echo " Checking Selinux ..."
-if [[ "$selinuxstatus" == "disabled" ]]
-then
-    echo " Selinux is set to $selinuxstatus"
-else
-    echo " Selinux is disabled"
-fi
-
-echo "  "
-echo "The current IP Address is ... $IP"
-if [[ `grep "$IP"  $webcl/index.php` ]]
-then
-   echo "Found $IP in $webcl/index.php"
-   grep "$IP"  $webcl/index.php
-else
-   echo "$IP not found in $webcl"
-   echo "Status is $? "
-fi
+var1="grep Reverse /etc/httpd/conf.d/i2b2_proxy.conf  | grep -oP '\d+\.\d+\.\d+\.\d+'"
+var2="grep urlCellPM /var/www/html/webclient/i2b2_config_data.js | grep -oP '\d+\.\d+\.\d+\.\d+'"
 
 echo " "
-echo "Checking ajp port in standalone.xml:"
-if [[ `grep "9009"  $wildf/standalone/configuration/standalone.xml` ]]
-then
-   echo "Found ajp 9009 in $wildf/standalone/configuration/standalone.xml"
-   grep "9009"  $wildf/standalone/configuration/standalone.xml
-else
-   echo "apj 9009 not found in standalone.xml"
-   echo "Status is $? "
-fi
+echo "/etc/httpd/conf.d/i2b2_proxy.com   ........  $var1"
+echo "/var/www/html/webclient/i2b2_config_data ..  $var2"
 
 echo " "
-echo "Checking ajp port in i2b2_proxy.conf"
-if [[ `grep "ajp"  $webht/i2b2_proxy.conf` ]]
-then
-   echo "Found ajp in $webht/i2b2_proxy.conf"
-  grep "ajp"  $webht/i2b2_proxy.conf
-else
-   echo "apj not found in $webht/i2b2_proxy.conf"
-fi
+echo "IP's in postgres i2b2pm.pm_cell_data field "
+echo " "
+psql  -d i2b2 -c 'select * from i2b2pm.pm_cell_data;' | grep -oP '\d+\.\d+\.\d+\.\d+'
+
--- scripts/i2b2-check.sh
+++
@@ -1,7 +0,0 @@
-grep CellPM /var/www/html/webclient/i2b2_config_data.js
-
-cat /etc/httd/conf.d/i2b2_proxy.com
-
-sestatus | grep Current
-
-grep -A 6 array /var/www/html/webclient/index.php
