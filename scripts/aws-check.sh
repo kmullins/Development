aafda1bba347db19a7b2a215f9b8aea30756d594
kmullins <kmmmullins@gmail.com>
2018-01-29 15:20:13 -0500

    Updating aws-check

--- scripts/aws-check.sh
+++ scripts/aws-check.sh
@@ -1,10 +1,19 @@
+#!/bin/bash
+################
+
+# New stuff
 
 
-
-for x in ec2-user@52.91.118.56  ec2-user@52.201.221.251 root@184.73.215.50 ec2-user@107.20.249.75 
+for x in ec2-user@52.91.118.56  ec2-user@52.201.221.251 ec2-user@184.73.215.50 centos@107.20.249.75 
 do
      ssh  -i /home/kmullins/secret/i2b2key.pem $x "uname -a && df -h & top -b | head -n 5  "
     echo "   "
 
 done     
      
+for x in ubuntu@52.207.245.82  ubunut@52.200.164.25 centos@34.228.157.32 ubuntu@54.86.76.232 ubuntu@52.23.207.190 ubuntu@52.200.132.108 ubuntu@23.23.65.7 ubuntu@52.86.153.239 centos@50.16.141.243
+do
+     ssh  -i /home/kmullins/secret/transmart-key-pair.pem $x "uname -a && df -h & top -b | head -n 5  "
+    echo "   "
+
+done     
